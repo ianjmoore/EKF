c3830e683c6f5f25124c32f9d15d006ee6414067
ianjmoore <ian.j.moore95@gmail.com>
2022-12-29 17:22:55 -0500

    plotting attempt

--- 1d_plant/1d_plant.py
+++ 1d_plant/1d_plant.py
@@ -1,39 +1,60 @@
-# from numpy import sqrt
-# import math
-# from matplotlib import pyplot
+import numpy as np
+import time
+import matplotlib.pyplot as plt
 
 
-# parameters
+## parameters
 
 x_0 = 0 # initial pos (m)
 v_0 = 1 # initial vel (m/s)
-a   = 0.1 # accel (m/s**2)
-dt  = 0.01 # delta time (s)
+a   = float(0.1) # accel (m/s**2)
+dt  = float(0.01) # delta time (s)
 
 t_f = 10 # time final (s)
 
-# state initialization
+## state initialization
 x = x_0
 v = v_0
 t = 0
 
+# enable interactive mode
+plt.ion()
+
+# creating subplot and figure
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line1, = ax.plot(t, x)
+
+# setting labels
+plt.xlabel("Time (s)")
+plt.ylabel("Position (m)")
+plt.title("Position wrt Time")
 
 for t in range(int(t_f/dt)):
     # x = x_0 + v_0*t + 0.5*a*t**2
     # v = a*t + v_0
     # x = v*t + x_0
 
-    # Forward Euler Discrete Time Integration
-        # discrete time causes errors due to non-time constant errors
-        # decreasing dt will help mitigate these errors
+    ## Forward Euler Discrete Time Integration
+        # Discrete time causes errors due to non-time constant errors.
+        # Decreasing dt will help mitigate these errors
     v += a*dt
     x += v*dt
-
     t += dt
+
+    # updating the value of x and y
+    line1.set_xdata(t)
+    line1.set_ydata(x)
+
+    # re-drawing the figure
+    fig.canvas.draw()
+
+    # to flush the GUI events
+    fig.canvas.flush_events()
+    # time.sleep(0.1)
 
     print(f'Position is: {x} m')
     print(f'Velocity is: {v} m/s')
-
 
 
 '''
